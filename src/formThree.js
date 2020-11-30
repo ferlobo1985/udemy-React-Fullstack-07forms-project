b0dff43cfc8961019da598076c4bc5b1aba6454a
ferlobo1985 <fernando.lobo.prez@gmail.com>
2020-11-30 01:51:02 -0300

    Formik custom fields

--- src/formThree.js
+++ src/formThree.js
@@ -5,16 +5,39 @@ import * as Yup from 'yup';
 const FormThree = () => {
 
     const formikProps = {
-        initialValues:{ firstname:'', color:''},
+        initialValues:{ firstname:'', color:'', lastname:'',age:''},
         validationSchema:Yup.object({
-            firstname: Yup
-            .string()
-            .required('Sorry, this is required')
+            firstname: Yup.string().required('Sorry, this is required'),
+            lastname: Yup.string().required('Sorry, this is required'),
+            age: Yup.string().required('Sorry, this is required')
         }),
         onSubmit: values => {
             console.log(values)
         }
     }
+
+    const formikInputCustomField = ({
+        field, // {name, value, onChange, onBlur}
+        form: { touched, errors },
+        ...props
+    }) => (
+        <>
+            <label htmlFor={field.name}>{props.labelName}</label>
+            <input
+                type="text"
+                className="form-control"
+                placeholder={props.placeholder}
+                {...field}
+            />
+            { errors[field.name] && touched[field.name] ?
+                <span>{errors[field.name]}</span>
+            :null
+            }
+        </>
+       
+    )
+
+    
 
 
     return(
@@ -43,6 +66,23 @@ const FormThree = () => {
                                 <option value="blue">Blue</option>
                             </Field>
                             
+                            <hr className="mb-4" />
+
+                            <Field
+                                name="lastname"
+                                component={formikInputCustomField}
+                                placeholder="Last name"
+                                labelName="Enter your lastname"
+                            />
+
+                            <hr className="mb-4" />
+                            <Field
+                                name="age"
+                                component={formikInputCustomField}
+                                placeholder="put your age"
+                                labelName="Enter your age"
+                            />
+
                             <hr className="mb-4" />
                             <button className="btn btn-primary btn-lg btn-block" type="submit">
                                 Submit
