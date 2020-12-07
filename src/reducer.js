e04352072207a4aa9d5dc7ddc2b3f95a1a52c0d2
ferlobo1985 <fernando.lobo.prez@gmail.com>
2020-12-07 15:45:03 -0300

    The useReducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,35 @@
-import React from  'react';
+import React, { useReducer } from  'react';
+import { reducerFile } from './utils/reducerFile';
 
 const Reducer = () => {
+    const [state,dispatch] = useReducer(reducerFile,{ user:'Steve',count:0,clicks:0})
+    
+    const changeNameHandler = () => {
+        const newName = ['MIke','Ron','Steve'];
+        return {
+            type:"change_name", 
+            payload: newName[Math.floor(Math.random()* newName.length)]
+        }
+    }
+
+
     return(
-        <>
-            reducer
-        </>
+        <div className="container">
+            <div>
+                Current user: {state.user}
+            </div>
+            <div>
+                Current clicks: {state.clicks}
+            </div>
+            <div>
+                Current count: {state.count}
+            </div>
+            <button onClick={()=> dispatch({type:"increment",payload:1})}>Increment</button>
+            <button onClick={()=> dispatch({type:"decrement",payload:1})}>Decrement</button>
+            <button onClick={()=> dispatch(changeNameHandler())}>Change name</button>
+
+        </div>
     )
 }
 
-export const Reducer;
+export default Reducer;
